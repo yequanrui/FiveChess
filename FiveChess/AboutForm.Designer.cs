f10dca09c6c55bd68dd7be568208f6866a7ea3f2
yequanrui <yequanrui@qq.com>
2017-04-04 18:31:04 +0800

    rebuild files

---
+++ FiveChess/AboutForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FiveChess
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_close = new System.Windows.Forms.Button();
+            this.lbl_tool = new System.Windows.Forms.Label();
+            this.lbl_author = new System.Windows.Forms.Label();
+            this.lbl_about = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_close.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_close.Location = new System.Drawing.Point(92, 158);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(70, 29);
+            this.btn_close.TabIndex = 5;
+            this.btn_close.Text = "关闭";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // lbl_tool
+            // 
+            this.lbl_tool.AutoSize = true;
+            this.lbl_tool.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_tool.Location = new System.Drawing.Point(23, 126);
+            this.lbl_tool.Name = "lbl_tool";
+            this.lbl_tool.Size = new System.Drawing.Size(210, 22);
+            this.lbl_tool.TabIndex = 4;
+            this.lbl_tool.Text = "编程工具：VS2015（C＃）";
+            // 
+            // lbl_author
+            // 
+            this.lbl_author.AutoSize = true;
+            this.lbl_author.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_author.Location = new System.Drawing.Point(23, 70);
+            this.lbl_author.Name = "lbl_author";
+            this.lbl_author.Size = new System.Drawing.Size(90, 22);
+            this.lbl_author.TabIndex = 2;
+            this.lbl_author.Text = "作者：小叶";
+            // 
+            // lbl_about
+            // 
+            this.lbl_about.AutoSize = true;
+            this.lbl_about.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_about.Location = new System.Drawing.Point(88, 9);
+            this.lbl_about.Name = "lbl_about";
+            this.lbl_about.Size = new System.Drawing.Size(74, 22);
+            this.lbl_about.TabIndex = 0;
+            this.lbl_about.Text = "关于游戏";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_name.Location = new System.Drawing.Point(23, 42);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(95, 22);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "C#版五子棋";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_time.Location = new System.Drawing.Point(23, 98);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(208, 22);
+            this.lbl_time.TabIndex = 3;
+            this.lbl_time.Text = "编写时间：2013年6月24日";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(250, 200);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_tool);
+            this.Controls.Add(this.lbl_author);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.lbl_about);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "关于游戏";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label lbl_tool;
+        private System.Windows.Forms.Label lbl_author;
+        private System.Windows.Forms.Label lbl_about;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_time;
+    }
+}
---
+++ FiveChess/AboutForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FiveChess
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
