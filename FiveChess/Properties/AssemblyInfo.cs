ac02eee6fa286fff67b2b1e7e26cceed913ffe51
yequanrui <yequanrui@qq.com>
2023-12-30 18:16:54 +0800

    update

--- FiveChess/Properties/AssemblyInfo.cs
+++ FiveChess/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
 //请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("193bc7a0-1a20-4ddb-a903-8f18ca220c61")]
